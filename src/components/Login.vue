5d4b9620db29c1716aaa2a9adddcfe92907ea4a9
Recklesslmz <936838918@qq.com>
2017-07-11 17:31:07 +0800

    登录界面的修改

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,12 +4,29 @@
       <div class="content">
         <div class="signUp">
           <div>Do you have account?</div>
-          <button>LOGIN IN</button>
+          <button>SIGN UP</button>
         </div>
         <div class="loginIn">
           <div>Have an account!</div>
           <button>LOGIN IN</button>
         </div>
+      </div>
+      <div class="contentWrite">
+        <div class="content-title">
+          SIGN UP
+        </div>
+        <div class="signMain">
+          <div class="fullName">
+            <input type="text" placeholder="FullName"/>
+          </div>
+          <div class="Email">
+            <input type="email" placeholder="E-mail"/>
+          </div>
+          <div class="passWord">
+            <input type="password" placeholder="Password"/>
+          </div>
+        </div>
+        <button class="signBtn">SIGN UP</button>
       </div>
     </div>
   </content-container>
@@ -32,11 +49,9 @@
   }
 
   .content {
-    position: absolute;
-    top: 25%;
-    left: 27%;
-    width: 35rem;
-    height: 19rem;
+    margin: 10% auto;
+    width: 70%;
+    height: 20rem;
     background: #31393e;
     display: flex;
     .signUp {
@@ -77,4 +92,58 @@
 
   }
 
+  .contentWrite {
+    position: absolute;
+    margin: auto;
+    height: 25rem;
+    width: 30%;
+    top: 12%;
+    left: 20%;
+    background: #fff;
+    .signMain {
+      .fullName, .Email, .passWord {
+        width: 80%;
+        margin: {
+          left: 10%;
+          top: 2rem;
+        }
+      ;
+        input {
+          width: 100%;
+          height: 2.5rem;
+          border-bottom: 1px solid #d9d9d9;
+        }
+      }
+    }
+    .signBtn {
+      background: #009cfe;
+      color: #fff;
+      border: none;
+      width: 7.5rem;
+      height: 2.5rem;
+      border: {
+        radius: .2rem;
+      }
+    ;
+      margin: {
+        top: 3rem;
+        right: 2rem;
+      }
+    ;
+      float: right;
+    }
+    .content-title {
+      color: #009cfe;
+      margin: {
+        top: 2rem;
+        left: 1rem;
+      }
+      font: {
+        size: 1.5rem;
+        weight: 300;
+      }
+    ;
+
+    }
+  }
 </style>
