64f4d6b1f7203b7eb0e8fd95abf245ecd7e471d7
Recklesslmz <936838918@qq.com>
2017-07-11 21:03:43 +0800

    完善登录界面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,29 +4,31 @@
       <div class="content">
         <div class="signUp">
           <div>Do you have account?</div>
-          <button>SIGN UP</button>
+          <button @click="signUp">SIGN UP</button>
         </div>
         <div class="loginIn">
           <div>Have an account!</div>
-          <button>LOGIN IN</button>
+          <button @click="loginIn">LOGIN IN</button>
         </div>
       </div>
-      <div class="contentWrite">
+      <div :class="[{contentWrite:chooseIndex === 1},{contentWrite2:chooseIndex === 2}]">
         <div class="content-title">
-          SIGN UP
+          <span v-if="chooseIndex === 1">SIGN UP</span>
+          <span v-if="chooseIndex === 2">LOGIN IN</span>
         </div>
         <div class="signMain">
           <div class="fullName">
-            <input type="text" placeholder="FullName"/>
+            <input v-if="chooseIndex === 1" type="text" placeholder="FullName"/>
           </div>
           <div class="Email">
             <input type="email" placeholder="E-mail"/>
           </div>
           <div class="passWord">
-            <input type="password" placeholder="Password"/>
+            <input type="password" autocomplete="new-password" placeholder="Password"/>
           </div>
         </div>
-        <button class="signBtn">SIGN UP</button>
+        <button v-if="chooseIndex === 1" class="signBtn">SIGN UP</button>
+        <button v-if="chooseIndex === 2" class="signBtn">LOGIN IN</button>
       </div>
     </div>
   </content-container>
@@ -36,7 +38,17 @@
   import ContentContainer from './partials/ContentContainer.vue'
   export default {
     data () {
-      return {}
+      return {
+        chooseIndex: 1
+      }
+    },
+    methods: {
+      signUp(){
+        this.chooseIndex = 1
+      },
+      loginIn(){
+        this.chooseIndex = 2
+      }
     },
     components: {
       ContentContainer,
@@ -100,6 +112,8 @@
     top: 12%;
     left: 20%;
     background: #fff;
+    transition: all .475s;
+    transform: translate3d(0, 0, 0);
     .signMain {
       .fullName, .Email, .passWord {
         width: 80%;
@@ -112,6 +126,7 @@
           width: 100%;
           height: 2.5rem;
           border-bottom: 1px solid #d9d9d9;
+          background: #fff;
         }
       }
     }
@@ -120,7 +135,7 @@
       color: #fff;
       border: none;
       width: 7.5rem;
-      height: 2.5rem;
+      height: 2rem;
       border: {
         radius: .2rem;
       }
@@ -136,7 +151,7 @@
       color: #009cfe;
       margin: {
         top: 2rem;
-        left: 1rem;
+        left: 2.1rem;
       }
       font: {
         size: 1.5rem;
@@ -145,5 +160,10 @@
     ;
 
     }
+  }
+
+  .contentWrite2 {
+    @extend .contentWrite;
+    transform: translate3d(100%, 0, 0);
   }
 </style>
