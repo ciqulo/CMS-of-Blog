93cfa05a4916831f8ca3eb1e1202f1223778d5ad
Recklesslmz <936838918@qq.com>
2017-07-12 09:10:53 +0800

    修复登录的样式
    完善登录界面
    
    本地修改登录

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,11 +3,11 @@
     <div class="content">
       <div class="signUp">
         <div>Do you have account?</div>
-        <button @click="signUp">SIGN UP</button>
+        <button @click="signUpTab">SIGN UP</button>
       </div>
       <div class="loginIn">
         <div>Have an account!</div>
-        <button @click="loginIn">LOGIN IN</button>
+        <button @click="loginInTab">LOGIN IN</button>
       </div>
     </div>
     <div :class="[{contentWrite:chooseIndex === 1},{contentWrite2:chooseIndex === 2}]">
@@ -17,47 +17,94 @@
       </div>
       <div class="signMain">
         <div class="fullName">
-          <input v-if="chooseIndex === 1" type="text" placeholder="FullName"/>
+          <input v-if="chooseIndex === 1" v-model="fullName" type="text" placeholder="FullName"/>
         </div>
         <div class="Email">
-          <input type="email" placeholder="E-mail"/>
+          <input type="email" v-model="userName" placeholder="E-mail"/>
         </div>
         <div class="passWord">
-          <input type="password" autocomplete="new-password" placeholder="Password"/>
+          <input type="password" v-model="passWord" autocomplete="new-password" placeholder="Password"/>
         </div>
       </div>
-      <button v-if="chooseIndex === 1" class="signBtn">SIGN UP</button>
-      <button v-if="chooseIndex === 2" class="signBtn">LOGIN IN</button>
+      <button v-if="chooseIndex === 1" class="signBtn" @click="signUp">SIGN UP</button>
+      <button v-if="chooseIndex === 2" class="signBtn" @click="loginIn">LOGIN IN</button>
     </div>
+
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import {mapActions} from 'vuex'
+  import {mapActions, mapState} from 'vuex'
   export default {
     data () {
       return {
-        chooseIndex: 1
+        chooseIndex: 2,
+        fullName: '',
+        userName: '',
+        passWord: '',
       }
     },
+    created(){
+    },
     methods: {
-      signUp(){
+      signUpTab(){
         this.chooseIndex = 1
       },
-      loginIn(){
+      loginInTab(){
         this.chooseIndex = 2
-        this.LOGIN({name: 'ciqu', password: 111})
+
+      },
+      signUp(){
+        this.$message.error('注册你妹啊');
+      },
+      loginIn(){
+        if (!this.userName) {
+          this.$notify.info({
+            title: '提示',
+            message: '用户名不能为空'
+          });
+        } else if (!this.passWord) {
+          this.$notify.info({
+            title: '提示',
+            message: '密码不能为空'
+          });
+        } else {
+          this.LOGIN({name: this.userName, password: this.passWord})
+          setTimeout(() => {
+            this.$store.dispatch('LOGIN')
+            const userRep = this.user.user.user
+            if (userRep.info !== '登录成功') {
+              console.log(userRep)
+              this.$notify({
+                title: '警告',
+                message: userRep.info,
+                type: 'warning'
+              });
+            } else {
+              this.$router.push({path: '/'})
+            }
+            console.log(this.user.user)
+          }, 300)
+
+        }
       },
       ...mapActions(['LOGIN'])
     },
-    created(){
-
+    computed: {
+      ...mapState({
+        user: state => state.user
+      })
     }
+
   }
 </script>
 <style lang="scss">
   .login {
+    width: 100%;
+    height: 100%;
+    position: absolute;
     background: url("http://oqjgod7s1.bkt.clouddn.com/c10471e1ff97e45032194a111ea7ab80.jpg");
+    background-size: cover;
   }
 
   .content {
